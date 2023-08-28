1d71620df472e8e01f0ba3273feb402b3d25b752
lucasb-silva <lucasbeserrasilva18@gmail.com>
2023-08-28 12:04:34 -0300

    novo endpoint de teste criado

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello, world!")
+// O mircro framework Express transforma app em servidor HTTP
+
+// Importa e cria uma aplicão usando express
+const express = require('express')
+const app = express()
+
+// Criando o endpoint e retornando mensagem
+app.get('/', function (req, res) {
+  res.send('Hello World')
+});
+
+// Exercicio, criando novo endpoin 'oi'
+app.get("/oi", function (req, res) {
+    res.send('Olá, mundo')
+});
+
+// Aplicação ouvindo na porta
+app.listen(3000)
