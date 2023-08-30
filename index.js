17ab172ad19d13b279d04dd6cdad0dee79787809
lucasb-silva <lucasbeserrasilva18@gmail.com>
2023-08-30 11:27:12 -0300

    Banco de dados integrado com

--- index.js
+++ index.js
@@ -1,81 +1,105 @@
 // O mircro framework Express transforma app em servidor HTTP
 
-// Importa e cria uma aplicão usando express
-const express = require('express')
-const app = express()
+// Importa o express
+const express = require('express');
 
-// Habilitamos o processamento de JSON
-app.use(express.json());
 
-// Criando o endpoint e retornando mensagem
-app.get('/', function (req, res) {
-  res.send('Hello World')
-});
+const { MongoClient } = require("mongodb");
 
-// Exercicio, criando novo endpoin 'oi'
-app.get("/oi", function (req, res) {
-    res.send('Olá, mundo')
-});
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "jornada-backend-agosto-23";
+const client = new MongoClient(url);
 
-// Endpoint podcasts
-const lista = ["DevPira", "Hipsters.Tech", "Nerdcast"];
+async function main() {
+    console.info("Conectando ao banco de dados...");
+    await client.connect();
+    console.info("Banco de dados conectado com sucesso!");
 
-// Read All -> [GET] /podcasts
-app.get("/podcasts", function(req, res){
-    res.send(lista.filter(Boolean));
-});
+    const db = client.db(dbName);
+    const collection = db.collection("podcasts");
 
-// Create -> [POST] /podcasts
-app.post("/podcasts", function (req, res){
-    //console.log(req.body, typeof req.body);
-    
-    // Extraindo o nome do Body da Request (Corpo da Requisição)
-    const item = req.body.nome;
+    // Aplicação utilizando express
+    const app = express();
 
-    // Inserindo o item na lista
-    lista.push(item);
+    // Habilitamos o processamento de JSON
+    app.use(express.json());
 
-    // Enviando uma resposta de sucesso
-    res.send("Item criado com sucesso");
+    // Criando o endpoint e retornando mensagem
+    app.get('/', function (req, res) {
+    res.send('Hello World')
+    });
 
-});
+    // Exercicio, criando novo endpoin 'oi'
+    app.get("/oi", function (req, res) {
+        res.send('Olá, mundo')
+    });
 
-// Read By Id -> [GET] /podcasts/:id
-app.get("/podcasts/:id", function (req, res){
-    // Pegando o parâmetro de rota ID
-    const id = req.params.id - 1;
+    // Endpoint podcasts
+    const lista = ["DevPira", "Hipsters.Tech", "Nerdcast"];
 
-    // Pegando a informação da lista
-    const item = lista[id];
+    // Read All -> [GET] /podcasts
+    app.get("/podcasts", async function(req, res){
+        
+        const itens = await collection.find().toArray();
+        
+        res.send(itens);
+    });
 
-    // Exibindo o item na resposta do endpoint
-    res.send(item);
-});
+    // Create -> [POST] /podcasts
+    app.post("/podcasts", function (req, res){
+        //console.log(req.body, typeof req.body);
+        
+        // Extraindo o nome do Body da Request (Corpo da Requisição)
+        const item = req.body.nome;
 
-// Update -> [PUT] /podcasts/:id
-app.put("/podcasts/:id", function (req, res){
-    // Pegando o parâmetro de rota ID
-    const id = req.params.id - 1;
+        // Inserindo o item na lista
+        lista.push(item);
 
-    // Extraindo o nome do Body da Request (Corpo da Requisição)
-    const item = req.body.nome;
+        // Enviando uma resposta de sucesso
+        res.send("Item criado com sucesso");
 
-    // Atualizando a informação na lista de registros
-    lista[id] = item;
+    });
 
-    res.send("Item editado com sucesso!");
-})
+    // Read By Id -> [GET] /podcasts/:id
+    app.get("/podcasts/:id", function (req, res){
+        // Pegando o parâmetro de rota ID
+        const id = req.params.id - 1;
 
-// Delete -> [DELETE] /podcasts/:id
-app.delete("/podcasts/:id", function (req, res){
-    // Pegando o parâmetro de rota ID
-    const id = req.params.id - 1;
+        // Pegando a informação da lista
+        const item = lista[id];
 
-    // Excluindo o item da lista
-    delete lista[id];
+        // Exibindo o item na resposta do endpoint
+        res.send(item);
+    });
 
-    res.send("Item excluído com sucesso!");
-});
+    // Update -> [PUT] /podcasts/:id
+    app.put("/podcasts/:id", function (req, res){
+        // Pegando o parâmetro de rota ID
+        const id = req.params.id - 1;
 
-// Aplicação ouvindo na porta 3000
-app.listen(3000)
+        // Extraindo o nome do Body da Request (Corpo da Requisição)
+        const item = req.body.nome;
+
+        // Atualizando a informação na lista de registros
+        lista[id] = item;
+
+        res.send("Item editado com sucesso!");
+    })
+
+    // Delete -> [DELETE] /podcasts/:id
+    app.delete("/podcasts/:id", function (req, res){
+        // Pegando o parâmetro de rota ID
+        const id = req.params.id - 1;
+
+        // Excluindo o item da lista
+        delete lista[id];
+
+        res.send("Item excluído com sucesso!");
+    });
+
+    // Aplicação ouvindo na porta 3000
+    app.listen(3000);
+
+}
+
+main();
